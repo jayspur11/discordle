4828a52272b98cbd59d3340457cee384d6d44d40
Jaysen Spurlock <jayspur11@gmail.com>
2022-01-24 21:11:24 -0800

    No, wait, throw an exception

--- app.py
+++ app.py
@@ -6,5 +6,4 @@ from dotenv import load_dotenv
 # Set up the environment from `.env`
 load_dotenv()
 if "DISCORD_BOT_TOKEN" not in os.environ:
-    logging.log(logging.CRITICAL,
-                "DISCORD_BOT_TOKEN must be defined in the environment.")
+    raise RuntimeError("DISCORD_BOT_TOKEN must be defined in the environment.")
