4f1f8b426663bb4054a358a82751bbb3d5aef482
Jaysen Spurlock <jayspur11@gmail.com>
2022-01-24 21:11:52 -0800

    Run very minimal bot

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import discord
 import logging
 import os
 
@@ -7,3 +8,24 @@ from dotenv import load_dotenv
 load_dotenv()
 if "DISCORD_BOT_TOKEN" not in os.environ:
     raise RuntimeError("DISCORD_BOT_TOKEN must be defined in the environment.")
+
+
+class Discordle(discord.Client):
+    def __init__(self):
+        intents = discord.Intents.none()
+        intents.guilds = True
+        intents.members = True
+        intents.guild_messages = True
+        super().__init__(intents=intents)
+        
+    # Event handlers
+    async def on_ready(self):
+        logging.info("Discordle ready!")
+        
+    async def on_message(self, message: discord.Message):
+        print(message.content)
+
+
+if __name__ == "__main__":
+    bot = Discordle()
+    bot.run(os.environ["DISCORD_BOT_TOKEN"])
